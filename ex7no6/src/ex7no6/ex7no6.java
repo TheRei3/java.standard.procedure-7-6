96599b7f758be3c7813214d5d243171d72b1b497
정현우 <80958519+TheRei3@users.noreply.github.com>
2021-08-04 09:08:57 +0900

    ex7no6

---
+++ ex7no6/src/ex7no6/ex7no6.java
@@ -0,0 +1,22 @@
+package ex7no6;
+
+public class ex7no6 {  //  330p
+	public static void main(String[] args) {
+		Child c = new Child();
+		c.method();
+	}
+
+}
+class Parent {
+	int x = 10;
+}
+
+class Child extends Parent{
+	int x=20;
+	
+	void method() {
+		System.out.println("x = " +x );
+		System.out.println("this.x = " +this.x );
+		System.out.println("super.x = " +super.x );
+	}
+}
